cc4d08829ecf4abdaa6948289098feb29327a390
Aditya Gupta <ag15035@gmail.com>
2022-09-22 12:31:52 +0530

    main: Daemonise notice notifier

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod handlers;
 use handlers::*;
 
 pub use std::io::Error;
+use std::{thread::sleep, time::Duration};
 
 use notify_rust::Notification;
 use webpage::{Webpage, WebpageOptions};
@@ -37,44 +38,48 @@ pub fn get_html(url: &str) -> Result<String, Error> {
     Ok(webpage.http.body)
 }
 
-fn main() {
-    let sources = [
-        (
-            SourceName::BiharUGMAC22,
-            "https://bceceboard.bihar.gov.in/UGMACIndex.php",
-            //"https://bceceboard.bihar.gov.in/UGMAC22_Aplindex.php",
-        ),
-        (
-            SourceName::MCC,
-            "https://mcc.nic.in/WebinfoUG/Page/Page?PageId=1&LangId=P",
-        ),
-    ];
+fn main() -> ! {
+    loop {
+        let sources = [
+            (
+                SourceName::BiharUGMAC22,
+                "https://bceceboard.bihar.gov.in/UGMACIndex.php",
+                //"https://bceceboard.bihar.gov.in/UGMAC22_Aplindex.php",
+            ),
+            (
+                SourceName::MCC,
+                "https://mcc.nic.in/WebinfoUG/Page/Page?PageId=1&LangId=P",
+            ),
+        ];
 
-    let mut all_notices = Vec::new();
-    for src in sources {
-        match handle_source(src) {
-            Ok(mut notices) => all_notices.append(&mut notices),
-            Err(e) => {
-                println!("Failed to fetch {:?}. Error: {:?}", src, e);
+        let mut all_notices = Vec::new();
+        for src in sources {
+            match handle_source(src) {
+                Ok(mut notices) => all_notices.append(&mut notices),
+                Err(e) => {
+                    println!("Failed to fetch {:?}. Error: {:?}", src, e);
+                }
             }
         }
-    }
 
-    if all_notices.is_empty() {
-        println!("No new notices");
-        return;
-    }
+        if all_notices.is_empty() {
+            println!("No new notices");
+        }
 
-    for notice in all_notices {
-        println!("\n--------------------------------------");
-        println!("{}", notice.heading);
-        println!("Link   : {}", notice.link);
-        println!("Source : {:?}", notice.source);
-        println!("--------------------------------------\n");
+        for notice in all_notices {
+            println!("\n--------------------------------------");
+            println!("{}", notice.heading);
+            println!("Link   : {}", notice.link);
+            println!("Source : {:?}", notice.source);
+            println!("--------------------------------------\n");
 
-        Notification::new()
-            .summary(&notice.heading)
-            .body(&notice.link)
-            .icon("/usr/share/icons/hicolor/scalable/apps/ibus.svg");
+            Notification::new()
+                .summary(&notice.heading)
+                .body(&notice.link)
+                .icon("/usr/share/icons/hicolor/scalable/apps/ibus.svg");
+        }
+
+        // Sleep for 5 minutes
+        sleep(Duration::from_secs(5 * 60));
     }
 }
